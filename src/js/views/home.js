d137aac9efd35de8b2f7b07159afe78bccd8962e
SantaGosu <taz.c.munoz@gmail.com>
2022-09-21 14:41:47 +0000

    added card into home.js and gave it styling for page

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<div className="card">
+		<img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Grey.PNG" className="card-img-top"  />
+		<div className="card-body">
+		<h5 className="card-title">Card title</h5>
+			This is some text within a card body.
+		</div>
+		<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		<a href="#" className="btn btn-outline-primary card-btn">Go somewhere</a>
+		</div>
 	</div>
 );
